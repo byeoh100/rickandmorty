0dfcd00a8f44014f2e187f9b3080b9f704c099f5
byeoh100 <rax745@gmail.com>
2024-07-02 13:27:42 -0500

    Add favorites functionality

--- rickandmorty/src/pages/CharactersPage.jsx
+++ rickandmorty/src/pages/CharactersPage.jsx
@@ -3,13 +3,16 @@ import axios from 'axios'
 import Card from 'react-bootstrap/Card'
 import "./CharactersPage.css"
 import { useNavigate } from 'react-router-dom'
+import star from '../assets/star.png'
+import star_filled from '../assets/star_filled.png'
 
 function CharactersPage() {
     const [charList, setCharList] = useState([])
     const [page, setPage] = useState(1)
+    const [favorites, setFavorites] = useState([])
     const navigate = useNavigate()
 
-    console.log(charList)
+    console.log(favorites)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,11 +23,23 @@ function CharactersPage() {
     }, [page])
 
     const handlePage = (move) => {
-        if(move == "prev" && page > 1) {
+        if (move == "prev" && page > 1) {
             setPage(page - 1)
         }
-        else if(move == "next") {
+        else if (move == "next") {
             setPage(page + 1)
+        }
+    }
+
+    const handleFavorites = (id) => {
+        if (favorites.includes(id)) {
+            setFavorites(favorites.filter((i) => i != id))
+        }
+        else if (favorites.length < 4) {
+            setFavorites([...favorites, id])
+        }
+        else {
+            alert("You can only have up to 4 favorites")
         }
     }
 
@@ -42,10 +57,13 @@ function CharactersPage() {
             <div className="cardBox">
                 {charList.map((i, index) => (
                     <Card style={{ width: '18rem' }} key={index}>
-                        <Card.Img variant="top" src={i.image} onClick={() => navigate(`/character-details/${i.id}`)} style={{cursor : "pointer"}}/>
+                        <Card.Img variant="top" src={i.image} onClick={() => navigate(`/character-details/${i.id}`)} style={{ cursor: "pointer" }} />
                         <Card.Body>
                             <Card.Title>{i.name}</Card.Title>
-                            <button>Add to favorites</button>
+                            <img
+                                onClick={() => handleFavorites(i.id)}
+                                src={favorites.includes(i.id) ? star_filled : star}
+                                style={{ width: "1rem", height: "1rem", cursor: "pointer" }} />
                         </Card.Body>
                     </Card>
                 ))}
